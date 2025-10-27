5a757f8de9938860eed6a6bf8f45fa186bd11bc2
Bianca Sava <biancasava07@gmail.com>
2025-10-27 10:55:16 +0000

    BusStationRepository, BusRepository, BusTripRepository, RouteRepository created

---
+++ src/main/java/com/example/busmanagementsystem/repository/BusTripRepository.java
@@ -0,0 +1,44 @@
+package com.example.busmanagementsystem.repository;
+import com.example.busmanagementsystem.model.BusTrip;
+import java.util.List;
+
+public class BusTripRepository implements CRUD<BusTrip> {
+
+    List<BusTrip> busTrips;
+
+    @Override
+    public boolean create(BusTrip entity) {
+        return busTrips.add(entity);
+    }
+
+    @Override
+    public List<BusTrip> findAll() {
+        return busTrips;
+    }
+
+    @Override
+    public BusTrip findById(String id) {
+        for (BusTrip busTrip : busTrips) {
+            if (busTrip.getId().equals(id)) {
+                return busTrip;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean update(String id, BusTrip entity) {
+        for (int i = 0; i < busTrips.size(); i++) {
+            if (busTrips.get(i).getId().equals(id)) {
+                busTrips.set(i, entity);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return busTrips.removeIf(busTrip -> busTrip.getId().equals(id));
+    }
+}
---
+++ src/main/java/com/example/busmanagementsystem/repository/RouteRepository.java
@@ -0,0 +1,44 @@
+package com.example.busmanagementsystem.repository;
+import com.example.busmanagementsystem.model.Route;
+import java.util.List;
+
+public class RouteRepository implements CRUD<Route> {
+
+    private List<Route> routes;
+
+    @Override
+    public boolean create(Route entity) {
+        return routes.add(entity);
+    }
+
+    @Override
+    public List findAll() {
+        return routes;
+    }
+
+    @Override
+    public Route findById(String id) {
+        for (Route route : routes) {
+            if (route.getId().equals(id)) {
+                return route;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean update(String id, Route entity) {
+        for (int i = 0; i < routes.size(); i++) {
+            if (routes.get(i).getId().equals(id)) {
+                routes.set(i, (Route) entity);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return routes.removeIf(route -> route.getId().equals(id));
+    }
+}
