d324b79b87311d70660d381753673627c17b9bc3
Bianca Sava <biancasava07@gmail.com>
2025-10-27 09:55:18 +0000

    Created classes Passenger, Ticket and completed class DutyAssignment

---
+++ src/main/java/com/example/busmanagementsystem/model/DutyAssignment.java
@@ -0,0 +1,84 @@
+package com.example.busmanagementsystem.model;
+
+enum  Role {
+    PRIMARY_DRIVER("Primary Driver"),
+    RESERVE_DRIVER("Reserve  Driver");
+
+    private final String roleName;
+
+    Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+}
+
+
+public class DutyAssignment {
+    private String id;
+    private String tripId;
+    private String staffId;
+    private Role role;
+    /// Constructors
+    public DutyAssignment(String id, String tripId, String staffId, Role role) {
+        this.id = id;
+        this.tripId = tripId;
+        this.staffId = staffId;
+        this.role = role;
+    }
+    /// If no role is specified, default to RESERVE_DRIVER
+    public DutyAssignment(String id, String tripId, String staffId) {
+        this.id = id;
+        this.tripId = tripId;
+        this.staffId = staffId;
+        this.role = Role.RESERVE_DRIVER;
+    }
+
+    /// Getters and Setters
+    // region
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTripId() {
+        return tripId;
+    }
+
+    public void setTripId(String tripId) {
+        this.tripId = tripId;
+    }
+
+    public String getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(String staffId) {
+        this.staffId = staffId;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    //endregion
+
+
+    @Override
+    public String toString() {
+        return "DutyAssignment{" +
+                "id='" + id + '\'' +
+                ", tripId='" + tripId + '\'' +
+                ", staffId='" + staffId + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
