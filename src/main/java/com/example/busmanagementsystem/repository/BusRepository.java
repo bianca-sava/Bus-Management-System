e9ca2e2ef3db0d14e84cd24c9078b1447ec1c269
Bianca Sava <biancasava07@gmail.com>
2025-11-07 17:07:32 +0200

    Implement file-based repository for bus management, add JSON data files, and configure ObjectMapper for serialization

--- src/main/java/com/example/busmanagementsystem/repository/BusRepository.java
+++ src/main/java/com/example/busmanagementsystem/repository/BusRepository.java
@@ -1,13 +1,19 @@
 package com.example.busmanagementsystem.repository;
 import com.example.busmanagementsystem.model.Bus;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public class BusRepository extends  InMemoryRepository<Bus>{
+public class BusRepository extends  InFileRepository<Bus>{
+
+    protected BusRepository(ObjectMapper objectMapper) {
+        super("/Users/biancasava/IdeaProjects/Bus Management System/src/main/resources/data/bus.json", objectMapper,  Bus.class);
+    }
 
     @Override
     protected String getIdFromEntity(Bus entity) {
         return entity.getId();
     }
+
 }
