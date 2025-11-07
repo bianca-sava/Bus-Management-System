624332d4711df30132fc3b0d4c3f58a15014fe85
Bianca Sava <biancasava07@gmail.com>
2025-11-07 17:55:21 +0200

    Add JSON data files for buses, bus stations, trips, drivers, duty assignments, passengers, routes, tickets, and trip managers

--- src/main/java/com/example/busmanagementsystem/repository/DutyAssignmentRepository.java
+++ src/main/java/com/example/busmanagementsystem/repository/DutyAssignmentRepository.java
@@ -3,6 +3,7 @@ package com.example.busmanagementsystem.repository;
 import com.example.busmanagementsystem.model.Bus;
 import com.example.busmanagementsystem.model.DutyAssignment;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +11,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class DutyAssignmentRepository extends InFileRepository<DutyAssignment> {
 
-    protected DutyAssignmentRepository(ObjectMapper objectMapper, @Value("${repository.filepath.dutyassignment}") String filePath ) {
-        super(filePath, objectMapper,  DutyAssignment.class);
+    @Autowired
+    public DutyAssignmentRepository(
+            ObjectMapper objectMapper,
+            @Value("${repository.filepath.dutyassignment}") String filePath
+    ) {
+        super(filePath, objectMapper, DutyAssignment.class);
     }
 
     @Override
