430fbfc94beaec1fa43b6333987675791579147d
Simon Raul-Alexandru <raulsimonalexas@yahoo.com>
2025-11-18 12:22:33 +0200

    view for the list attributes implemented for Driver and TripManager

--- src/main/java/com/example/busmanagementsystem/controller/DriverController.java
+++ src/main/java/com/example/busmanagementsystem/controller/DriverController.java
@@ -1,8 +1,10 @@
 package com.example.busmanagementsystem.controller;
 
 import com.example.busmanagementsystem.model.Driver;
+import com.example.busmanagementsystem.model.DutyAssignment;
 import com.example.busmanagementsystem.model.TripManager;
 import com.example.busmanagementsystem.service.DriverService;
+import com.example.busmanagementsystem.service.DutyAssignmentsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,10 +15,12 @@ import org.springframework.web.bind.annotation.*;
 public class DriverController {
 
     private final DriverService driverService;
+    private final DutyAssignmentsService dutyAssignmentsService;
 
     @Autowired
-    public DriverController(DriverService driverService) {
+    public DriverController(DriverService driverService,  DutyAssignmentsService dutyAssignmentsService) {
         this.driverService = driverService;
+        this.dutyAssignmentsService = dutyAssignmentsService;
     }
 
     @GetMapping
@@ -72,5 +76,61 @@ public class DriverController {
     public String deleteDriver(@PathVariable String id) {
         driverService.deleteDriver(id);
         return "redirect:/driver";
+    }
+
+    @GetMapping("/{id}/assignments")
+    public String getDriverAssignments(@PathVariable String id, Model model) {
+        Driver driver = driverService.getDriverById(id);
+
+        if(driver != null) {
+            model.addAttribute("assignments", driver.getAssignments());
+            model.addAttribute("drivarId", driver.getId());
+            model.addAttribute("driverName", driver.getName());
+
+            return "driver/assignments";
+        }
+
+        return "redirect:/driver";
+    }
+
+    @GetMapping("/{id}/assignments/new")
+    public String showAssignAssignmentForm(@PathVariable String id, Model model) {
+        Driver driver = driverService.getDriverById(id);
+
+        if(driver != null) {
+            model.addAttribute("driverId", id);
+            model.addAttribute("availableAssignments", dutyAssignmentsService.getAllAssignments().values());
+
+            return "driver/assignAssignment";
+        }
+
+        return "redirect:/driver";
+    }
+
+    @PostMapping("/{driverId}/assignments/add")
+    public String addAssignmentToDriver(@PathVariable String driverId, @RequestParam String selectedAssignmentId) {
+        Driver driver = driverService.getDriverById(driverId);
+        DutyAssignment assignmentToAdd = dutyAssignmentsService.getAssignmentById(selectedAssignmentId);
+
+        if(driver != null && assignmentToAdd != null) {
+            driver.getAssignments().add(assignmentToAdd);
+
+            driverService.updateDriver(driverId, driver);
+        }
+
+        return "redirect:/driver/" + driverId + "/assignments";
+    }
+
+    @PostMapping("/{driverId}/assignment/{assignmentId}/delete")
+    public String deleteAssignmentFromDriver(@PathVariable String driverId, @PathVariable String assignmentId) {
+        Driver driver = driverService.getDriverById(driverId);
+        DutyAssignment assignmentToDelete = dutyAssignmentsService.getAssignmentById(assignmentId);
+
+        if(driver != null && assignmentToDelete != null) {
+            driver.getAssignments().remove(assignmentToDelete);
+            driverService.updateDriver(driverId, driver);
+        }
+
+        return "redirect:/driver/" + driverId + "/assignments";
     }
 }
--- src/main/java/com/example/busmanagementsystem/controller/TripManagerController.java
+++ src/main/java/com/example/busmanagementsystem/controller/TripManagerController.java
@@ -1,6 +1,8 @@
 package com.example.busmanagementsystem.controller;
 
+import com.example.busmanagementsystem.model.DutyAssignment;
 import com.example.busmanagementsystem.model.TripManager;
+import com.example.busmanagementsystem.service.DutyAssignmentsService;
 import com.example.busmanagementsystem.service.TripManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/tripmanager")
 public class TripManagerController {
     private final TripManagerService tripManagerService;
+    public final DutyAssignmentsService dutyAssignmentsService;
 
     @Autowired
-    public TripManagerController(TripManagerService service) {
+    public TripManagerController(TripManagerService service, DutyAssignmentsService dutyAssignmentsService) {
         this.tripManagerService = service;
+        this.dutyAssignmentsService = dutyAssignmentsService;
     }
 
     @GetMapping
@@ -72,5 +76,61 @@ public class TripManagerController {
     public String deleteTripManager(@PathVariable String id) {
         tripManagerService.deleteTripManager(id);
         return "redirect:/tripmanager";
+    }
+
+    @GetMapping("/{id}/assignments")
+    public String getDriverAssignments(@PathVariable String id, Model model) {
+        TripManager tripManager = tripManagerService.getTripManagerById(id);
+
+        if(tripManager != null) {
+            model.addAttribute("assignments", tripManager.getAssignments());
+            model.addAttribute("tripManagerId", tripManager.getId());
+            model.addAttribute("tripManagerName", tripManager.getName());
+
+            return "tripmanager/assignments";
+        }
+
+        return "redirect:/tripmanager";
+    }
+
+    @GetMapping("/{id}/assignments/new")
+    public String showAssignAssignmentForm(@PathVariable String id, Model model) {
+         TripManager tripManager = tripManagerService.getTripManagerById(id);
+
+        if(tripManager != null) {
+            model.addAttribute("tripManagerId", id);
+            model.addAttribute("availableAssignments", dutyAssignmentsService.getAllAssignments().values());
+
+            return "tripmanager/assignAssignment";
+        }
+
+        return "redirect:/tripmanager";
+    }
+
+    @PostMapping("/{tripmanagerId}/assignments/add")
+    public String addAssignmentToDriver(@PathVariable String tripmanagerId, @RequestParam String selectedAssignmentId) {
+        TripManager tripManager = tripManagerService.getTripManagerById(tripmanagerId);
+        DutyAssignment assignmentToAdd = dutyAssignmentsService.getAssignmentById(selectedAssignmentId);
+
+        if(tripManager != null && assignmentToAdd != null) {
+            tripManager.getAssignments().add(assignmentToAdd);
+
+            tripManagerService.updateTripManager(tripmanagerId, tripManager);
+        }
+
+        return "redirect:/tripmanager/" + tripmanagerId + "/assignments";
+    }
+
+    @PostMapping("/{tripmanagerId}/assignment/{assignmentId}/delete")
+    public String deleteAssignmentFromDriver(@PathVariable String tripmanagerId, @PathVariable String assignmentId) {
+        TripManager tripManager = tripManagerService.getTripManagerById(tripmanagerId);
+        DutyAssignment assignmentToDelete = dutyAssignmentsService.getAssignmentById(assignmentId);
+
+        if(tripManager != null && assignmentToDelete != null) {
+            tripManager.getAssignments().remove(assignmentToDelete);
+            tripManagerService.updateTripManager(tripmanagerId, tripManager);
+        }
+
+        return "redirect:/tripmanager/" + tripmanagerId + "/assignments";
     }
 }
