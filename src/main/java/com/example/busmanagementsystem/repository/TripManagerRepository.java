7f15cf20b81b687ebb2ca6a1a1d5f320794f05a5
Simon Raul-Alexandru <raulsimonalexas@yahoo.com>
2025-11-22 19:53:53 +0200

    Repositories updated

--- src/main/java/com/example/busmanagementsystem/repository/TripManagerRepository.java
+++ src/main/java/com/example/busmanagementsystem/repository/TripManagerRepository.java
@@ -1,17 +1,21 @@
 package com.example.busmanagementsystem.repository;
 
-import com.example.busmanagementsystem.model.Bus;
 import com.example.busmanagementsystem.model.TripManager;
+import com.example.busmanagementsystem.repository.interfaces.TripManagerJpaRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TripManagerRepository extends InFileRepository<TripManager> {
+public class TripManagerRepository extends DatabaseRepository<TripManager> {
 
-    protected TripManagerRepository(ObjectMapper objectMapper, @Value("${repository.filepath.tripmanager}") String filePath ) {
-        super(filePath, objectMapper,  TripManager.class);
+    public TripManagerRepository(TripManagerJpaRepository jpaRepository) {
+        super(jpaRepository);
     }
+
+//    protected TripManagerRepository(ObjectMapper objectMapper, @Value("${repository.filepath.tripmanager}") String filePath ) {
+//        super(filePath, objectMapper,  TripManager.class);
+//    }
 
     @Override
     protected String getIdFromEntity(TripManager entity) {
--- src/main/java/com/example/busmanagementsystem/repository/interfaces/PassengerJpaRepository.java
+++ src/main/java/com/example/busmanagementsystem/repository/interfaces/PassengerJpaRepository.java
@@ -1,9 +1,10 @@
 package com.example.busmanagementsystem.repository.interfaces;
 import com.example.busmanagementsystem.model.Bus;
+import com.example.busmanagementsystem.model.Passenger;
 import com.example.busmanagementsystem.repository.PassengerRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PassengerJpaRepository extends JpaRepository<PassengerRepository, String> {
+public interface PassengerJpaRepository extends JpaRepository<Passenger, String> {
 }
