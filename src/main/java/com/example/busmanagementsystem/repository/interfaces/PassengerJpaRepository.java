8097407b2f8459e71a252fedf5bcf90a237dd081
Simon Raul-Alexandru <raulsimonalexas@yahoo.com>
2025-12-15 22:50:27 +0200

    more filters implemented

--- src/main/java/com/example/busmanagementsystem/repository/interfaces/PassengerJpaRepository.java
+++ src/main/java/com/example/busmanagementsystem/repository/interfaces/PassengerJpaRepository.java
@@ -4,12 +4,22 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PassengerJpaRepository extends JpaRepository<Passenger, String> {
-    @Query(value = "SELECT p, COUNT(t) AS ticketCount FROM Passenger p LEFT JOIN p.tickets t GROUP BY p",
-            countQuery = "SELECT COUNT(p) FROM Passenger p",
-            nativeQuery = false)
-    Page<Object[]> findAllSortedByTicketCount(Pageable pageable);
-}
+    @Query("SELECT p FROM Passenger p WHERE" +
+            "(:id IS NULL OR LOWER(p.id) LIKE LOWER(CONCAT('%', :id, '%'))) AND" +
+            "(:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))) AND" +
+            "(:currency IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :currency, '%'))) AND" +
+            "(:minTickets IS NULL OR SIZE(p.tickets) >= :minTickets) AND" +
+            "(:maxTickets IS NULL OR SIZE(p.tickets) <= :maxTickets)")
+    Page<Passenger> findByFilters(
+            @Param("id") String id,
+            @Param("name") String name,
+            @Param("currency") String currency,
+            @Param("minTickets") Integer minTickets,
+            @Param("maxTickets") Integer maxTickets,
+            Pageable pageable
+    );}
